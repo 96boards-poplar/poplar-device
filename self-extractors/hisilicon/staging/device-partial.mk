d5c56a87612f595bd4799af6507f9cb609764bd3
Bin Chen <bin.chen@linaro.org>
2018-01-16 12:10:02 +1100

    install hw video impl to vendor dir
    Signed-off-by: Bin Chen <bin.chen@linaro.org>

--- self-extractors/hisilicon/staging/device-partial.mk
+++ self-extractors/hisilicon/staging/device-partial.mk
@@ -15,12 +15,12 @@
 # Binary blob(s) from Hisilicon for poplar hardware
 # stagefright
 PRODUCT_COPY_FILES := \
-    $(LOCAL_PATH)/proprietary/libhi_common.so:system/lib/libhi_common.so \
-    $(LOCAL_PATH)/proprietary/libhi_msp.so:system/lib/libhi_msp.so \
-    $(LOCAL_PATH)/proprietary/libhi_vfmw.so:system/lib/libhi_vfmw.so \
-    $(LOCAL_PATH)/proprietary/libOMX_Core.so:system/lib/libOMX_Core.so \
-    $(LOCAL_PATH)/proprietary/libOMX.hisi.video.decoder.so:system/lib/libOMX.hisi.video.decoder.so \
-    $(LOCAL_PATH)/proprietary/libstagefrighthw.so:system/lib/libstagefrighthw.so \
+    $(LOCAL_PATH)/proprietary/libhi_common.so:system/vendor/lib/libhi_common.so \
+    $(LOCAL_PATH)/proprietary/libhi_msp.so:system/vendor/lib/libhi_msp.so \
+    $(LOCAL_PATH)/proprietary/libhi_vfmw.so:system/vendor/lib/libhi_vfmw.so \
+    $(LOCAL_PATH)/proprietary/libOMX_Core.so:system/vendor/lib/libOMX_Core.so \
+    $(LOCAL_PATH)/proprietary/libOMX.hisi.video.decoder.so:system/vendor/lib/libOMX.hisi.video.decoder.so \
+    $(LOCAL_PATH)/proprietary/libstagefrighthw.so:system/vendor/lib/libstagefrighthw.so \
     $(LOCAL_PATH)/proprietary/libhiavplayer.so:system/vendor/lib/libhiavplayer.so \
     $(LOCAL_PATH)/proprietary/libhiavplayer_adp.so:system/vendor/lib/libhiavplayer_adp.so \
     $(LOCAL_PATH)/proprietary/libhiavplayerservice.so:system/vendor/lib/libhiavplayerservice.so \
