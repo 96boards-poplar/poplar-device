af9c65a0a2060c46b428070daaa850abb69a8ee4
Bin Chen <bin.chen@linaro.org>
2018-02-06 10:16:43 +1100

    enable 64bit platform build
    the CPU for poplar is A53, use this to setup the ARCH/CPU/VARIANT
    correctly.
    
    Signed-off-by: Bin Chen <bin.chen@linaro.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,13 +36,22 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=poplar androidboot.selinux=permissi
 TARGET_NO_RECOVERY := true
 TARGET_NO_RADIOIMAGE := true
 
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a9
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_VARIANT := cortex-a15
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+
 
 TARGET_USES_64_BIT_BINDER := true
+TARGET_SUPPORTS_32_BIT_APPS := true
+TARGET_SUPPORTS_64_BIT_APPS := true
 
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
