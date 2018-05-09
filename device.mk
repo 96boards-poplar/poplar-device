22cc3e95ff8b47e83a6fc10d6bad87dfd4349927
Bin Chen <bin.chen@linaro.org>
2018-05-09 16:47:20 +1000

    sort everything by hal name
    Signed-off-by: Bin Chen <bin.chen@linaro.org>

--- device.mk
+++ device.mk
@@ -39,41 +39,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 
 
-# start HAL keymaster >>>>>>>>
-
-
-## build packages
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
-
-
-
-# start HAL graphics >>>>>>>>
-
-
-## build packages
-PRODUCT_PACKAGES += \
-    libion \
-    android.hardware.graphics.mapper@2.0 \
-    android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.graphics.allocator@2.0 \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1 \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
-## copy packages
-PRODUCT_COPY_FILES += \
-    vendor/hisilicon/poplar/proprietary/libGLES_mali.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libGLES_mali.so \
-    vendor/hisilicon/poplar/proprietary/hwcomposer.poplar.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/hwcomposer.poplar.so \
-    vendor/hisilicon/poplar/proprietary/libhi_gfx2d.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhi_gfx2d.so \
-    vendor/hisilicon/poplar/proprietary/liboverlay.so:$(TARGET_COPY_OUT_VENDOR)/lib/liboverlay.so \
-    vendor/hisilicon/poplar/proprietary/gralloc.poplar.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gralloc.poplar.so \
-    vendor/hisilicon/poplar/proprietary/libion_ext.so:$(TARGET_COPY_OUT_VENDOR)/lib/libion_ext.so \
-
-
-
 # start HAL audio >>>>>>>>
 ## feature declaration
 PRODUCT_COPY_FILES += \
@@ -93,48 +58,6 @@ PRODUCT_PACKAGES += \
 ## runtime configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-
-
-
-# start HAL media.codec >>>>>>>>
-
-
-## copy packages
-PRODUCT_COPY_FILES += \
-    vendor/hisilicon/poplar/proprietary/libstagefrighthw.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefrighthw.so \
-    vendor/hisilicon/poplar/proprietary/libhi_common.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhi_common.so \
-    vendor/hisilicon/poplar/proprietary/libhi_msp.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhi_msp.so \
-    vendor/hisilicon/poplar/proprietary/libhi_vfmw.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhi_vfmw.so \
-    vendor/hisilicon/poplar/proprietary/libOMX_Core.so:$(TARGET_COPY_OUT_VENDOR)/lib/libOMX_Core.so \
-    vendor/hisilicon/poplar/proprietary/libOMX.hisi.video.decoder.so:$(TARGET_COPY_OUT_VENDOR)/lib/libOMX.hisi.video.decoder.so \
-    vendor/hisilicon/poplar/proprietary/libhiavplayer.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhiavplayer.so \
-    vendor/hisilicon/poplar/proprietary/libhiavplayer_adp.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhiavplayer_adp.so \
-    vendor/hisilicon/poplar/proprietary/libhiavplayerservice.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhiavplayerservice.so \
-    vendor/hisilicon/poplar/proprietary/hiavplayer:$(TARGET_COPY_OUT_VENDOR)/bin/hiavplayer \
-## service init.rc scripts
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/hiavplayer.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hiavplayer.rc \
-## runtime configs
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-
-
-
-# start HAL drm >>>>>>>>
-
-
-## build packages
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-# raw instructions - do I have a better place to go?
-ifeq ($(TARGET_TEE_IS_OPTEE), true)
-   $(call inherit-product-if-exists, device/hisilicon/poplar/optee-packages.mk)
-endif
-## service init.rc scripts
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/optee.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/optee.rc \
 
 
 
@@ -164,6 +87,83 @@ PRODUCT_COPY_FILES += \
 ## service init.rc scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bt_mkt.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/bt_mkt.rc \
+
+
+
+# start HAL drm >>>>>>>>
+
+
+## build packages
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+# raw instructions - do I have a better place to go?
+ifeq ($(TARGET_TEE_IS_OPTEE), true)
+   $(call inherit-product-if-exists, device/hisilicon/poplar/optee-packages.mk)
+endif
+## service init.rc scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/optee.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/optee.rc \
+
+
+
+# start HAL graphics >>>>>>>>
+
+
+## build packages
+PRODUCT_PACKAGES += \
+    libion \
+    android.hardware.graphics.mapper@2.0 \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.allocator@2.0 \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1 \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+## copy packages
+PRODUCT_COPY_FILES += \
+    vendor/hisilicon/poplar/proprietary/libGLES_mali.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libGLES_mali.so \
+    vendor/hisilicon/poplar/proprietary/hwcomposer.poplar.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/hwcomposer.poplar.so \
+    vendor/hisilicon/poplar/proprietary/libhi_gfx2d.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhi_gfx2d.so \
+    vendor/hisilicon/poplar/proprietary/liboverlay.so:$(TARGET_COPY_OUT_VENDOR)/lib/liboverlay.so \
+    vendor/hisilicon/poplar/proprietary/gralloc.poplar.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gralloc.poplar.so \
+    vendor/hisilicon/poplar/proprietary/libion_ext.so:$(TARGET_COPY_OUT_VENDOR)/lib/libion_ext.so \
+
+
+
+# start HAL keymaster >>>>>>>>
+
+
+## build packages
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service \
+
+
+
+# start HAL media.codec >>>>>>>>
+
+
+## copy packages
+PRODUCT_COPY_FILES += \
+    vendor/hisilicon/poplar/proprietary/libstagefrighthw.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefrighthw.so \
+    vendor/hisilicon/poplar/proprietary/libhi_common.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhi_common.so \
+    vendor/hisilicon/poplar/proprietary/libhi_msp.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhi_msp.so \
+    vendor/hisilicon/poplar/proprietary/libhi_vfmw.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhi_vfmw.so \
+    vendor/hisilicon/poplar/proprietary/libOMX_Core.so:$(TARGET_COPY_OUT_VENDOR)/lib/libOMX_Core.so \
+    vendor/hisilicon/poplar/proprietary/libOMX.hisi.video.decoder.so:$(TARGET_COPY_OUT_VENDOR)/lib/libOMX.hisi.video.decoder.so \
+    vendor/hisilicon/poplar/proprietary/libhiavplayer.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhiavplayer.so \
+    vendor/hisilicon/poplar/proprietary/libhiavplayer_adp.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhiavplayer_adp.so \
+    vendor/hisilicon/poplar/proprietary/libhiavplayerservice.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhiavplayerservice.so \
+    vendor/hisilicon/poplar/proprietary/hiavplayer:$(TARGET_COPY_OUT_VENDOR)/bin/hiavplayer \
+## service init.rc scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/hiavplayer.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hiavplayer.rc \
+## runtime configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 
 
 
