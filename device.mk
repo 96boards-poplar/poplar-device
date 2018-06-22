29db545fe916e9ffc48afef8e4c85569a38b7406
Shawn Guo <shawn.guo@linaro.org>
2018-06-22 15:00:41 +0800

    device.mk: build soundtrigger package for audio support
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- device.mk
+++ device.mk
@@ -51,6 +51,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio.usb.default \
     audio.r_submix.default \
